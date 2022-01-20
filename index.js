58094950669e785609fe5da9213850b3dfa21ed9
AbdulmajeedS <abdulmajeed.sabriye777@gmai.com>
2022-01-20 23:06:33 +0000

    Coordinates by IP

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { fetchMyIP } = require('./iss');
+const { fetchCoordsByIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+fetchCoordsByIP('174.94.0.87', (error, coords) => {
+  // if (error) {
+  //   console.log("It didn't work!" , error);
+  //   return;
+  // }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+  console.log('It worked! Returned Coords:' , coords);
+}); 
